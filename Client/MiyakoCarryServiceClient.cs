eada43746a01f4e8317db64e833fac560f7f47cc
姫様の夢 <285835609@qq.com>
2026-02-04 12:22:18 +0800

    修改版本号

--- Client/MiyakoCarryServiceClient.cs
+++ Client/MiyakoCarryServiceClient.cs
@@ -20,7 +20,7 @@ namespace MiyakoCarryService.Client;
 [BepInProcess("EscapeFromTarkov.exe")]
 public sealed class MiyakoCarryServicePlugin : BaseUnityPlugin
 {
-    public const string BepInExClientVersion = "0.1.2.0";
+    public const string BepInExClientVersion = "0.1.3.0";
     public static Version ClientVersion { get; } = new(BepInExClientVersion);
     public const string McsGUID = "top.himesamanoyume.miyakocarryservice";
     public const string McsPluginName = "姫様の夢 MiyakoCarryService";
