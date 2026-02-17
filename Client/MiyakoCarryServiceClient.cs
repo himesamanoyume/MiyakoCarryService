5a231ebb3fb387f6bcb4f201667137e2b970ab1d
姫様の夢 <285835609@qq.com>
2026-02-17 14:53:40 +0800

    修改版本号

--- Client/MiyakoCarryServiceClient.cs
+++ Client/MiyakoCarryServiceClient.cs
@@ -25,7 +25,7 @@ namespace MiyakoCarryService.Client;
 [BepInDependency(McsFikaGUID, BepInDependency.DependencyFlags.SoftDependency)]
 public sealed class MiyakoCarryServicePlugin : BaseUnityPlugin
 {
-    public const string BepInExClientVersion = "0.1.5.6";
+    public const string BepInExClientVersion = "0.1.5.7";
     public static Version ClientVersion { get; } = new(BepInExClientVersion);
     public const string McsGUID = "top.himesamanoyume.miyakocarryservice";
     public const string FikaGUID = "com.fika.core";
