33144c5b0d7188a45bc0330261172d7cb757e1a3
姫様の夢 <285835609@qq.com>
2026-03-17 22:56:40 +0800

    小调整

--- Server/ModMetadata.cs
+++ Server/ModMetadata.cs
@@ -1,27 +1,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using SPTarkov.Server.Core.Models.Spt.Mod;
+using SPTarkov.Server.Web;
 
 namespace MiyakoCarryService.Server
 {
-        public record ModMetadata : AbstractModMetadata
-        {
-                private const string CurrentVersion = "0.1.9.0";
+    public record ModMetadata : AbstractModMetadata, IModWebMetadata
+    {
+        private const string CurrentVersion = "0.1.9.0";
 #if CHEATERCARRY
-                public override string Name { get; init; } = "MiyakoCarryServiceServer 红护版";
+        public override string Name { get; init; } = "MiyakoCarryServiceServer 红护版";
 #else
-                public override string Name { get; init; } = "MiyakoCarryServiceServer";
+        public override string Name { get; init; } = "MiyakoCarryServiceServer";
 #endif
-                public override string Author { get; init; } = "Himesamanoyume";
-                public override List<string> Contributors { get; init; }
-                public override SemanticVersioning.Version Version { get; init; } = new(string.Join('.', CurrentVersion.Split('.', System.StringSplitOptions.None).Take(3)));
-                public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
-                public override List<string> Incompatibilities { get; init; }
-                public override Dictionary<string, SemanticVersioning.Range> ModDependencies { get; init; }
-                public override string Url { get; init; } = "https://sns.oddba.cn/184517.html";
-                public override bool? IsBundleMod { get; init; } = false;
-                public override string License { get; init; } = "CC BY-NC-SA 4.0";
-                public override string ModGuid { get; init; } = "top.himesamanoyume.miyakocarryservice";
-                public System.Version ClientVersion { get; init; } = new(CurrentVersion);
-        }
+        public override string Author { get; init; } = "Himesamanoyume";
+        public override List<string> Contributors { get; init; }
+        public override SemanticVersioning.Version Version { get; init; } = new(string.Join('.', CurrentVersion.Split('.', System.StringSplitOptions.None).Take(3)));
+        public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
+        public override List<string> Incompatibilities { get; init; }
+        public override Dictionary<string, SemanticVersioning.Range> ModDependencies { get; init; }
+        public override string Url { get; init; } = "https://sns.oddba.cn/184517.html";
+        public override bool? IsBundleMod { get; init; } = false;
+        public override string License { get; init; } = "CC BY-NC-SA 4.0";
+        public override string ModGuid { get; init; } = "top.himesamanoyume.miyakocarryservice";
+        public System.Version ClientVersion { get; init; } = new(CurrentVersion);
+    }
 }
--- Server/Services/CompatibilityService.cs
+++ Server/Services/CompatibilityService.cs
@@ -1,30 +1,39 @@
 
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.Models.Spt.Mod;
 
 
 namespace MiyakoCarryService.Server.Services
 {
     [Injectable(InjectionType.Singleton)]
     public sealed class CompatibilityService(
+        IReadOnlyList<SptMod> loadedMods
         // SptLogger<CompatibilityService> logger
     )
     {
         public bool HasFikaServer { get; private set; } = false;
+        public bool HasAPBS { get; private set; } = false;
         public Type FikaMatchServiceType { get; private set; } = null;
         public Type FikaMatchType { get; private set; } = null;
 
         public async Task OnPostLoadAsync()
         {
-            CheckFikaServerPlugins();
+            HasFikaServer = loadedMods.Any(mod => mod.ModMetadata.ModGuid == "Fika");
+            if (HasFikaServer)
+            {
+                CheckFikaServerType();
+            }
+            HasAPBS = loadedMods.Any(mod => mod.ModMetadata.ModGuid == "com.acidphantasm.progressivebotsystem");
         }
 
-        private void CheckFikaServerPlugins()
+        private void CheckFikaServerType()
         {
             FikaMatchServiceType = Type.GetType("FikaServer.Services.MatchService, FikaServer");
-            HasFikaServer = FikaMatchServiceType is not null;
             FikaMatchType = Type.GetType("FikaServer.Models.Fika.FikaMatch, FikaServer");
         }
     }
