9f94c1b60aca7a21faffb34aa2ef29af92db8c18
姫様の夢 <285835609@qq.com>
2026-07-06 16:48:53 +0800

    Client插件示例

---
+++ Examples/ClientAddon/MiyakoCarryServiceClientAddon.cs
@@ -0,0 +1,54 @@
+﻿
+
+using BepInEx;
+using EFT;
+using MiyakoCarryService.Client.Api;
+using MiyakoCarryService.Client.Enums;
+using MiyakoCarryService.Client.Models;
+
+namespace MiyakoCarryService.Client.Addon
+{
+    [BepInPlugin(McsPluginAddonName, MiyakoCarryServicePlugin.McsPluginName, MiyakoCarryServicePlugin.BepInExClientVersion)]
+    [BepInProcess(MiyakoCarryServicePlugin.EFTexe)]
+    [BepInDependency(MiyakoCarryServicePlugin.BigBrainGUID, BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency(MiyakoCarryServicePlugin.McsGUID, BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency(MiyakoCarryServicePlugin.FikaGUID, BepInDependency.DependencyFlags.SoftDependency)]
+    [BepInDependency(MiyakoCarryServicePlugin.McsFikaGUID, BepInDependency.DependencyFlags.SoftDependency)]
+    public sealed class MiyakoCarryServiceClientAddonPlugin : BaseUnityPlugin
+    {
+        public const string McsPluginAddonName = "top.himesamanoyume.miyakocarryservice.clientaddon";
+
+        void Start()
+        {
+            McsCommandApi.Register(EMenuId.Team.ToString(), BuildTestSubMenu);
+            McsCommandApi.Register(EMenuId.Member.ToString(), BuildMemberSubMenu);
+        }
+
+        private void BuildTestSubMenu(McsCommandMenu menu, Player[] mcsBotPlayers)
+        {
+            menu.RegisterSubMenu("测试子菜单1", "测试子菜单1说明", (m) => TestSubMenu(m, mcsBotPlayers));
+            menu.RegisterCommand("测试指令1", "测试指令1说明", TestCommandAction, mcsBotPlayers);
+        }
+
+        private void BuildMemberSubMenu(McsCommandMenu menu, Player[] mcsBotPlayers)
+        {
+            menu.RegisterSubMenu("测试子菜单2", "测试子菜单2说明", (m) => MemberSubMenu(m, mcsBotPlayers));
+            menu.RegisterCommand("测试指令3", "测试指令3说明", TestCommandAction, mcsBotPlayers);
+        }
+
+        private void TestSubMenu(McsCommandMenu menu, Player[] mcsBotPlayers)
+        {
+            menu.RegisterCommand("测试指令2", "测试指令2说明", TestCommandAction, mcsBotPlayers);
+        }
+
+        private void MemberSubMenu(McsCommandMenu menu, Player[] mcsBotPlayers)
+        {
+            menu.RegisterCommand("测试指令4", "测试指令4说明", TestCommandAction, mcsBotPlayers);
+        }
+
+        private void TestCommandAction(Player[] mcsBotPlayers, params object[] args)
+        {
+            McsCommandApi.CloseCommandMenuAction();
+        }
+    }
+}
