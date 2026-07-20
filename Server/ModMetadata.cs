f25fcf2c84a082633319c40697c1eb57b6eee81d
姫様の夢 <285835609@qq.com>
2026-07-20 08:51:16 +0800

    修改版本号

--- Server/ModMetadata.cs
+++ Server/ModMetadata.cs
@@ -7,7 +7,7 @@ namespace MiyakoCarryService.Server
 {
     public record ModMetadata : AbstractModMetadata, IModWebMetadata
     {
-        private const string CurrentVersion = "1.0.11.1";
+        private const string CurrentVersion = "1.0.12.0";
 #if DEBUG
         public override string Name { get; init; } = "MiyakoCarryServiceServer DebugBuild";
 #else
