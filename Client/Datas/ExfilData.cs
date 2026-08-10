2a7972df54f9245c8bfb8d3b373a268d04fc4d17
姫様の夢 <285835609@qq.com>
2026-08-10 19:36:08 +0800

    优化获取撤离点位置的方式

--- Client/Datas/ExfilData.cs
+++ Client/Datas/ExfilData.cs
@@ -4,12 +4,14 @@ using EFT.Interactive;
 using MiyakoCarryService.Client.Extensions;
 using MiyakoCarryService.Client.Utils;
 using UnityEngine;
+using Comfort.Common;
 
 namespace MiyakoCarryService.Client.Datas
 {
     public class ExfilData : TriggerData
     {
         private WeakReference<ExfiltrationPoint> _exfilRef;
+        private AIExfiltrationPoint _aiExfiltrationPoint;
         public ExfiltrationPoint ExfiltrationPoint => _exfilRef.TryGetTarget(out var exfil) ? exfil : null;
 
         public ExfilData(ExfiltrationPoint exfiltrationPoint) : base()
@@ -28,10 +30,51 @@ namespace MiyakoCarryService.Client.Datas
             _ => false
         };
 
+        public override Vector3 GetPos()
+        {
+            var aiExfiltrationPoint = FindAiExfiltrationPoint();
+            if (aiExfiltrationPoint != null)
+            {
+                return aiExfiltrationPoint.Position;
+            }
+            return base.GetPos();
+        }
+
+        private AIExfiltrationPoint FindAiExfiltrationPoint()
+        {
+            if (_aiExfiltrationPoint != null)
+            {
+                return _aiExfiltrationPoint;
+            }
+
+            var exfilPoint = ExfiltrationPoint;
+            if (exfilPoint == null || string.IsNullOrEmpty(exfilPoint.Settings.Name))
+            {
+                return null;
+            }
+
+            var aiExfiltrationPoints = Singleton<IBotGame>.Instance?.BotsController?.CoversData?.Patrols?.ExfiltrationPoints;
+            if (aiExfiltrationPoints == null)
+            {
+                return null;
+            }
+
+            foreach (var aiExfiltrationPoint in aiExfiltrationPoints)
+            {
+                if (aiExfiltrationPoint != null && aiExfiltrationPoint.Name == exfilPoint.Settings.Name)
+                {
+                    _aiExfiltrationPoint = aiExfiltrationPoint;
+                    break;
+                }
+            }
+            return _aiExfiltrationPoint;
+        }
+
         public override void Dispose()
         {
             base.Dispose();
             _exfilRef = null;
+            _aiExfiltrationPoint = null;
         }
     }
 }
