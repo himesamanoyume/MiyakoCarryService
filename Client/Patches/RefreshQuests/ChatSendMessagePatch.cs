33a5f141b7734c9df7da0d1dbbfbf340daf3bbd5
姫様の夢 <285835609@qq.com>
2026-08-10 23:34:59 +0800

    更加主动刷新任务

--- Client/Patches/RefreshQuests/ChatSendMessagePatch.cs
+++ Client/Patches/RefreshQuests/ChatSendMessagePatch.cs
@@ -6,22 +6,45 @@ using HarmonyLib;
 using MiyakoCarryService.Client.Events;
 using MiyakoCarryService.Client.Mgrs;
 using SPT.Reflection.Patching;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MiyakoCarryService.Client.Patches.RefreshQuests
 {
     /// <summary>
-    /// 当可能发送下单相关的消息时进行主动请求刷新任务
+    /// 发送任何给宫子商人的消息时，主动请求刷新任务
     /// </summary>
     public sealed class ChatSendMessagePatch : ModulePatch
     {
+        private static CancellationTokenSource _questRefreshCts;
+
         protected override MethodBase GetTargetMethod() => AccessTools.Method(typeof(ClientBackendSession), nameof(ClientBackendSession.ChatSendMessage));
 
         [PatchPostfix]
         public static void Postfix(string id, int type, string text, string replyTo, Callback<string> callback)
         {
-            if (text.Contains("mcs order") || text.Contains("mcs ticket"))
+            if (id != MiyakoCarryServicePlugin.MiyakoTraderId)
             {
+                return;
+            }
+
+            _questRefreshCts?.Cancel();
+            _questRefreshCts?.Dispose();
+            _questRefreshCts = new CancellationTokenSource();
+            TasksExtensions.HandleExceptions(DelayedRefreshAsync(_questRefreshCts.Token));
+        }
+
+        private static async Task DelayedRefreshAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken).ConfigureAwait(true);
                 EventMgr.Notify(new UpdateDailyQuestsEvent());
+            }
+            catch (OperationCanceledException)
+            {
+
             }
         }
     }
