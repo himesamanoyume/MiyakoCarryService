dabc26c22accd46da15f7af40aab8740ad7322dc
姫様の夢 <285835609@qq.com>
2026-08-29 03:06:39 +0800

    分离

--- Client/Extensions/BotBewareGrenadeExtensions.cs
+++ Client/Extensions/BotBewareGrenadeExtensions.cs
@@ -38,20 +38,4 @@ namespace MiyakoCarryService.Client.Extensions
             }
         }
     }
-
-    public static class GrenadeDangerPointExntensions
-    {
-        extension(GrenadeDangerPoint grenadeDangerPoint)
-        {
-            public bool McsShallRunAway()
-            {
-                if (grenadeDangerPoint.IsActive() && (grenadeDangerPoint._owner.Transform.position - grenadeDangerPoint.DangerPoint).sqrMagnitude <= 64f)
-                {
-                    return true;
-                }
-
-                return false;
-            }
-        }
-    }
 }
