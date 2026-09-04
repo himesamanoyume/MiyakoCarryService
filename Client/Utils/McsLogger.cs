acf61f191cff0189593aedfee913377df2c459c9
姫様の夢 <285835609@qq.com>
2026-09-04 20:24:07 +0800

    完善日志

--- Client/Utils/McsLogger.cs
+++ Client/Utils/McsLogger.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using BepInEx.Logging;
 
 namespace MiyakoCarryService.Client.Utils
@@ -52,7 +53,7 @@ namespace MiyakoCarryService.Client.Utils
                     continue;
                 }
 
-                segments.Add((method.DeclaringType, method.Name));
+                segments.Add(NormalizeFrame(method));
             }
 
             if (segments.Count == 0)
@@ -60,14 +61,107 @@ namespace MiyakoCarryService.Client.Utils
                 segments.Add((typeof(McsLogger), nameof(Log)));
             }
 
-            var chain = string.Join(" -> ", segments.Select((segment, index) =>
-            {
-                var typeName = index == 0 ? segment.DeclaringType.FullName ?? segment.DeclaringType.Name : segment.DeclaringType.Name;
-                return $"{typeName}.{segment.MethodName}()";
-            }).Reverse());
+            var logSiteSegment = segments[0];
+            var callerSegments = segments.Skip(1).ToList();
+
+            var orderedCallers = callerSegments.AsEnumerable().Reverse().Distinct().ToList();
+
+            var chain = BuildChain(orderedCallers, logSiteSegment);
 
             var result = $"[{chain}]\n{data}";
             _logger.Log(level, result);
+        }
+
+        private static string BuildChain(List<(Type DeclaringType, string MethodName)> orderedCallers, (Type DeclaringType, string MethodName) logSiteSegment)
+        {
+            var chainSegments = new List<string>(orderedCallers.Count + 1);
+
+            foreach (var callerSegment in orderedCallers)
+            {
+                chainSegments.Add($"{callerSegment.DeclaringType.Name}.{callerSegment.MethodName}()");
+            }
+
+            var logSiteTypeName = logSiteSegment.DeclaringType.FullName ?? logSiteSegment.DeclaringType.Name;
+            chainSegments.Add($"{logSiteTypeName}.{logSiteSegment.MethodName}()");
+
+            return string.Join(" -> ", chainSegments);
+        }
+
+        private static bool IsCompilerGeneratedFrame(MethodBase method)
+        {
+            if (method.Name.IndexOf('<') >= 0)
+            {
+                return true;
+            }
+
+            var declaringType = method.DeclaringType;
+            if (declaringType != null && declaringType.Name.IndexOf('<') >= 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static (Type DeclaringType, string MethodName) NormalizeFrame(MethodBase method)
+        {
+            var displayType = method.DeclaringType;
+
+            while (displayType != null && displayType.Name.IndexOf('<') >= 0)
+            {
+                displayType = displayType.DeclaringType;
+            }
+
+            if (!IsCompilerGeneratedFrame(method))
+            {
+                return (displayType ?? method.DeclaringType, method.Name);
+            }
+
+            var displayMethod = ExtractHostMethodName(method);
+            return (displayType ?? method.DeclaringType, displayMethod);
+        }
+
+        private static string ExtractHostMethodName(MethodBase method)
+        {
+            var hostMethodName = ExtractAngleToken(method.Name);
+            if (hostMethodName != null)
+            {
+                return hostMethodName;
+            }
+
+            if (method.DeclaringType != null)
+            {
+                hostMethodName = ExtractAngleToken(method.DeclaringType.Name);
+                if (hostMethodName != null)
+                {
+                    return hostMethodName;
+                }
+            }
+
+            return method.Name;
+        }
+
+        private static string ExtractAngleToken(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return null;
+            }
+
+            var start = name.IndexOf('<');
+            if (start < 0)
+            {
+                return null;
+            }
+
+            var end = name.IndexOf('>', start + 1);
+            if (end < 0)
+            {
+                return null;
+            }
+
+            var token = name.Substring(start + 1, end - start - 1);
+            return string.IsNullOrEmpty(token) ? null : token;
         }
 
         private static int GetMaxFrames(LogLevel level)
